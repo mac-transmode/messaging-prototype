37d8c3acfca94036c1f650f35eb0471da3a4c142
Martin Carlsson <martin.carlsson@transmode.se>
2015-02-21 18:53:12 +0100

    Added repo for springboot. FrontMain is now a Spring app.

--- front_main/src/main/java/se/ystrom/push/front/main/FrontMain.java
+++ front_main/src/main/java/se/ystrom/push/front/main/FrontMain.java
@@ -1,17 +1,21 @@
 package se.ystrom.push.front.main;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * The main for the frontend server serving the web ui and
  * the rest resources and websockets.
  */
+@SpringBootApplication
 public class FrontMain {
 
-    // hidden
-    private FrontMain() {
+    // Need to be visible for Spring
+    FrontMain() {
         // empty
     }
 
     public static void main(String[] args) {
-        new FrontMain();
+        SpringApplication.run(FrontMain.class, args);
     }
 }
