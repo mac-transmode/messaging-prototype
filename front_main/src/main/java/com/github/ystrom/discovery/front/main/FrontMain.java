a2c0470a81b06f38840829f0fec2cd2f9bbf34b2
Martin Carlsson <martin.carlsson@transmode.se>
2015-03-19 22:40:18 +0100

    Renamed package; added ref to the external filter

--- front_main/src/main/java/com/github/ystrom/discovery/front/main/FrontMain.java
+++ front_main/src/main/java/com/github/ystrom/discovery/front/main/FrontMain.java
@@ -1,13 +1,17 @@
-package se.ystrom.push.front.main;
+package com.github.ystrom.discovery.front.main;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import com.github.ystromm.springboot.webjars.WebjarVersionFilter;
 
 /**
  * The main for the frontend server serving the web ui and
  * the rest resources and websockets.
  */
 @SpringBootApplication
+@ComponentScan(basePackageClasses={WebjarVersionFilter.class})
 public class FrontMain {
 
     // Need to be visible for Spring
