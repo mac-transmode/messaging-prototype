d9dc708900e8a6b2b5aabcb2ba98b4056f9ebe84
Martin Carlsson <martin.carlsson@transmode.se>
2015-03-21 20:57:43 +0100

    Added google maps example

---
+++ front_main/src/main/resources/static/js/app.js
@@ -0,0 +1,58 @@
+angular.module('networkMap', [ 'uiGmapgoogle-maps' ]).controller(
+		'networkMapCtrl', function($scope) {
+			$scope.map = {
+				center : {
+					latitude : 59.30601,
+					longitude : 18.029238
+				},
+				zoom : 14
+			};
+			$scope.marker = {
+				id : 0,
+				coords : {
+					latitude : 59.30601,
+					longitude : 18.029238
+				},
+				icon : "/server11.png"
+			};
+			$scope.options = {
+				scrollwheel : false
+			};
+			$scope.polylines = [ {
+				id : 1,
+				path : [ { 
+					latitude : 59.4, longitude : 18.0
+				}, {
+					latitude : 59.3, longitude : 18.1
+				}, {
+					latitude : 59.1, longitude : 18.0
+				} ],
+				stroke : {
+					color : '#6060FB',
+					weight : 3
+				},
+				editable : true,
+				draggable : true,
+				geodesic : true,
+				visible : true
+			}, {
+				id : 2,
+				path : [ {
+					latitude : 59.4, longitude : 18.1
+				}, {
+					latitude : 59.3, longitude : 18.2
+				}, {
+					latitude : 59.3, longitude : 18.1
+				}, {
+					latitude : 59.2, longitude : 18.2
+				} ],
+				stroke : {
+					color : '#6060FB',
+					weight : 3
+				},
+				editable : true,
+				draggable : true,
+				geodesic : true,
+				visible : true
+			} ];
+		});
